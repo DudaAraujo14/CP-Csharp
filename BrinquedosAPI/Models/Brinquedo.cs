3184f029046b90ecd1d10d846bcf2abd0f4e6c5c
Duda.Araujo <eduarda.mpenas@gmail.com>
2026-03-25 19:54:59 -0300

    feat: adiciona model Brinquedo e DbContext

---
+++ BrinquedosAPI/Models/Brinquedo.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BrinquedosAPI.Models
+{
+    public class Brinquedo
+    {
+        [Key] 
+        public int Id_brinquedo { get; set; }
+
+        public string? Nome_brinquedo { get; set; }
+        public string? Tipo_brinquedo { get; set; }
+        public string? Classificacao { get; set; }
+        public string? Tamanho { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
